e55c6ee3dc4f01cb4f97e2cbed79186e7725c245
carissa morrow <carissamorrow@hotmail.com>
2019-01-07 09:07:04 -0700

    showing win

--- Program.cs
+++ Program.cs
@@ -2,11 +2,38 @@
 
 namespace randomNumber
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      int secret;
+      bool won = false;
+
+      Console.WriteLine("Let's play the numbers game!");
+      Random rand = new Random();
+      secret = rand.Next(1, 101);
+      Console.WriteLine(secret);
+      while (!won) ;
+      string guessString = Console.ReadLine();
+
+      int guess;
+      if (Int41.TryParse(guessString, out guess))
+      {
+        if (guess == secret)
         {
-            Console.WriteLine("Hello World!");
+          Console.WriteLine("You Win!!!!");
+          won = true;
         }
+        else if (guess > secret)
+        {
+          Console.WriteLine("Guess is too high");
+        }
+        else
+        {
+          Console.WriteLine("Guess is too low");
+        }
+      }
+
     }
+  }
 }
